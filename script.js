9d218ca32693e5c6a579542d69630d630d74b5d9
Soft Midi Player <168365564+kim10102233@users.noreply.github.com>
2025-01-20 15:44:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const textArray = ['ㅇ', '오', '온', '오느', '오늘', '오늘ㄷ', '오늘도', '오늘도 ', '오늘도 ㅎ', 
-        '오늘도 힘', '오늘도 힘ㄴ', '오늘도 힘ㄴ', '오늘도 힘냊', '오늘도 힘내자', '오늘도 힘내자.',
+        '오늘도 힘', '오늘도 힘ㄴ', '오늘도 힘ㄴ', '오늘도 힘내', '오늘도 힘냊', '오늘도 힘내자', '오늘도 힘내자.',
         '오늘도 힘내자..', '오늘도 힘내자..!', '오늘도 힘내자..! ', '오늘도 힘내자..! :', '오늘도 힘내자..! :3'];
     const typingText = document.getElementById('typing-text');
     const cursor = document.querySelector('.cursor');
@@ -52,24 +52,6 @@ document.addEventListener('DOMContentLoaded', () => {
             setTimeout(Meow, 100);
         }
     }
-
-    function createSpark(e) {
-        const spark = document.createElement('div');
-        spark.classList.add('spark');
-        document.body.appendChild(spark);
-
-        const rect = document.body.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-
-        spark.style.setProperty('--x', `${Math.random() * 100 - 50}px`);
-        spark.style.setProperty('--y', `${Math.random() * 100 - 50}px`);
-        setTimeout(() => {
-            spark.remove();
-        }, 1000);
-    }
-
-    document.body.addEventListener('click', createSpark);
 
     Meow();
 });
